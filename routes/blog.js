5823d9d2f57c5c22e6fa763b4a3a988d3c92aff5
chandanjha333 <162859024+chandanjha333@users.noreply.github.com>
2026-06-06 23:57:17 +0530

    added comment feature and finished project

--- routes/blog.js
+++ routes/blog.js
@@ -26,15 +26,18 @@ router.get('/add-new', (req, res) => {
 
 router.get('/:id', async (req, res) => {
   const blog = await Blog.findById(req.params.id).populate('createdBy');
+  const comments = await Comment.find({ blogId: req.params.id }).populate('createdBy');
   return res.render('blog', {
     user: req.user,
     blog,
+    comments,
   })
 });
 
-router.post('/comment/:blogId', async (req,res) => {
+router.post('/comment/:blogId', upload.none(), async (req, res) => {
+  const { content } = req.body;
   await Comment.create({
-    content: req.body.content,
+    content,
     blogId: req.params.blogId,
     createdBy: req.user._id,
   });
